fd886e2bd615884c29a1cb415cee08a2f1703208
Jaeeun98 <wodms4525@naver.com>
2020-04-15 17:41:43 +0900

    Title : Add
    Brush Size Add

--- Game/app.js
+++ Game/app.js
@@ -1,14 +1,17 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
-ctx.linewidth = 2.5;
+ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
 	painting = false;
@@ -35,6 +38,21 @@ function handleColorClick (event) {
 	ctx.strokeStyle = color;
 }
 
+function handleRangeChang(event) {
+	const size = event.target.value;
+	ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+	if(filling === true) {
+		filling = false;
+		mode.innerText = "Fill"
+	} else {
+		filling = true;
+		mode.innerText = "Faint"
+	}
+}
+
 if(canvas){
 	canvas.addEventListener("mousemove", onMouseMove);
 	canvas.addEventListener("mousedown", startPainting);
@@ -42,4 +60,14 @@ if(canvas){
 	canvas.addEventListener("mouseleave", stopPainting);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+Array.from(colors).forEach(color => 
+	color.addEventListener("click", handleColorClick)
+	);
+
+if(colors) {
+	range.addEventListener("input", handleRangeChang)
+}
+
+if(mode) {
+	mode.addEventListener("click", handleModeClick);
+}
