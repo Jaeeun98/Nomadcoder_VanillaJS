fdb878b025c4051738e0e4eb6c54f9816406954f
Jaeeun98 <wodms4525@naver.com>
2020-04-27 15:35:09 +0900

    Title : Add
    JS modal Add(display_none)

--- gallery/index.js
+++ gallery/index.js
@@ -10,7 +10,6 @@ const modal_2 = document.querySelector(".modal_2");
 const modal_3 = document.querySelector(".modal_3");
 const modal_4 = document.querySelector(".modal_4");
 const modal_5 = document.querySelector(".modal_5");
-const modalAll = document.querySelector(".modal");
 const modal = [modal_1, modal_2, modal_3, modal_4, modal_5];
 
 function handleModal4(){
@@ -29,15 +28,31 @@ function handleModal0(){
 	modal[0].style.display = "block";
 }
 
+function handleNon4(){
+	modal[4].style.display = "none";
+}
+function handleNon3(){
+	modal[3].style.display = "none";
+}
+function handleNon2(){
+	modal[2].style.display = "none";
+}
+function handleNon1(){
+	modal[1].style.display = "none";
+}
+function handleNon0(){
+	modal[0].style.display = "none";
+}
+
 slide[4].addEventListener("click", handleModal4);
 slide[3].addEventListener("click", handleModal3);
 slide[2].addEventListener("click", handleModal3);
 slide[1].addEventListener("click", handleModal3);
 slide[0].addEventListener("click", handleModal3);
 
-//modalAll.addEventListener("click", handleNon);
-
-
-
-
+modal[4].addEventListener("click", handleNon4);
+modal[3].addEventListener("click", handleNon3);
+modal[2].addEventListener("click", handleNon2);
+modal[1].addEventListener("click", handleNon1);
+modal[0].addEventListener("click", handleNon0);
 
