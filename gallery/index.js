df30bd09969b0f294d93382da7dcd6b9352c3c49
Jaeeun98 <wodms4525@naver.com>
2020-04-28 17:24:19 +0900

    Title : change
    JS modal change

--- gallery/index.js
+++ gallery/index.js
@@ -1,72 +1,66 @@
-const slide_1 = document.querySelector(".slide_1");
-const slide_2 = document.querySelector(".slide_2");
-const slide_3 = document.querySelector(".slide_3");
-const slide_4 = document.querySelector(".slide_4");
-const slide_5 = document.querySelector(".slide_5");
-const slide = [slide_1, slide_2, slide_3, slide_4, slide_5];
+const slideImg  = document.getElementsByClassName("slideImg");
+const slide = document.querySelector(".slide");
 
-const modal_1 = document.querySelector(".modal_1");
-const modal_2 = document.querySelector(".modal_2");
-const modal_3 = document.querySelector(".modal_3");
-const modal_4 = document.querySelector(".modal_4");
-const modal_5 = document.querySelector(".modal_5");
-const modal = [modal_1, modal_2, modal_3, modal_4, modal_5];
+const modalImg = document.getElementsByClassName("modalImg")
 
 const rightBtn = document.querySelector(".right");
 const leftBtn = document.querySelector(".left");
+const slideIn = 0;
 
-console.log(modal.length);
+slide.lenght = slideIn;
+console.log(slide);
 
 /*modal*/
 
+
 function handleModal4(){
-	modal[4].style.display = "block";
+	modalImg[4].style.display = "block";
 }
 function handleModal3(){
-	modal[3].style.display = "block";
+	modalImg[3].style.display = "block";
 }
 function handleModal2(){
-	modal[2].style.display = "block";
+	modalImg[2].style.display = "block";
 }
 function handleModal1(){
-	modal[1].style.display = "block";
+	modalImg[1].style.display = "block";
 }
 function handleModal0(){
-	modal[0].style.display = "block";
+	modalImg[0].style.display = "block";
 }
 
 function handleNon4(){
-	modal[4].style.display = "none";
+	modalImg[4].style.display = "none";
 }
 function handleNon3(){
-	modal[3].style.display = "none";
+	modalImg[3].style.display = "none";
 }
 function handleNon2(){
-	modal[2].style.display = "none";
+	modalImg[2].style.display = "none";
 }
 function handleNon1(){
-	modal[1].style.display = "none";
+	modalImg[1].style.display = "none";
 }
 function handleNon0(){
-	modal[0].style.display = "none";
+	modalImg[0].style.display = "none";
 }
 
-slide[4].addEventListener("click", handleModal4);
-slide[3].addEventListener("click", handleModal3);
-slide[2].addEventListener("click", handleModal3);
-slide[1].addEventListener("click", handleModal3);
-slide[0].addEventListener("click", handleModal3);
+slideImg[4].addEventListener("click", handleModal4);
+slideImg[3].addEventListener("click", handleModal3);
+slideImg[2].addEventListener("click", handleModal2);
+slideImg[1].addEventListener("click", handleModal1);
+slideImg[0].addEventListener("click", handleModal0);
 
-modal[4].addEventListener("click", handleNon4);
-modal[3].addEventListener("click", handleNon3);
-modal[2].addEventListener("click", handleNon2);
-modal[1].addEventListener("click", handleNon1);
-modal[0].addEventListener("click", handleNon0);
+modalImg[4].addEventListener("click", handleNon4);
+modalImg[3].addEventListener("click", handleNon3);
+modalImg[2].addEventListener("click", handleNon2);
+modalImg[1].addEventListener("click", handleNon1);
+modalImg[0].addEventListener("click", handleNon0);
 
 /*btn slide*/
 
-function rightSlide(){
+function handleslide(){
 
 }
 
-rightBtn.addEventListener("click", rightSlide);
+rightBtn.addEventListener("click", handleslide)
