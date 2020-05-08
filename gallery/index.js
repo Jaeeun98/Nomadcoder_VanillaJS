370fd3b19815d558081dac960e376ec64110c923
Jaeeun98 <wodms4525@naver.com>
2020-05-08 16:29:48 +0900

    Title : Change
    Modal JS Change

--- gallery/index.js
+++ gallery/index.js
@@ -2,7 +2,9 @@ const slideImg  = document.getElementsByClassName("slideImg");
 const slide = document.querySelector(".slide");
 const slideLe = slideImg.length;
 
-const modalImg = document.getElementsByClassName("modalImg")
+const modalImg = document.getElementsByClassName("modalImg");
+const modal = document.querySelector(".modal");
+const modalBtn = document.getElementsByClassName("modalBtn");
 
 const rightBtn = document.querySelector(".right");
 const leftBtn = document.querySelector(".left");
@@ -13,37 +15,29 @@ let slideIn = 1;
 
 /*modal*/
 
-
 function handleModal4(){
+	modal.style.display = "block";
 	modalImg[4].style.display = "block";
 }
 function handleModal3(){
+	modal.style.display = "block";
 	modalImg[3].style.display = "block";
 }
 function handleModal2(){
+	modal.style.display = "block";
 	modalImg[2].style.display = "block";
 }
 function handleModal1(){
+    modal.style.display = "block";
 	modalImg[1].style.display = "block";
 }
 function handleModal0(){
+	modal.style.display = "block";
 	modalImg[0].style.display = "block";
 }
 
-function handleNon4(){
-	modalImg[4].style.display = "none";
-}
-function handleNon3(){
-	modalImg[3].style.display = "none";
-}
-function handleNon2(){
-	modalImg[2].style.display = "none";
-}
-function handleNon1(){
-	modalImg[1].style.display = "none";
-}
-function handleNon0(){
-	modalImg[0].style.display = "none";
+function handleNon(){
+	modal.style.display = "none";
 }
 
 slideImg[4].addEventListener("click", handleModal4);
@@ -52,11 +46,11 @@ slideImg[2].addEventListener("click", handleModal2);
 slideImg[1].addEventListener("click", handleModal1);
 slideImg[0].addEventListener("click", handleModal0);
 
-modalImg[4].addEventListener("click", handleNon4);
-modalImg[3].addEventListener("click", handleNon3);
-modalImg[2].addEventListener("click", handleNon2);
-modalImg[1].addEventListener("click", handleNon1);
-modalImg[0].addEventListener("click", handleNon0);
+modalBtn[4].addEventListener("click", handleNon)
+modalBtn[3].addEventListener("click", handleNon)
+modalBtn[2].addEventListener("click", handleNon)
+modalBtn[1].addEventListener("click", handleNon)
+modalBtn[0].addEventListener("click", handleNon)
 
 /*btn slide*/
 
